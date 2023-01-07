75bbab9bff18952c4bf891c33ab675fe050ca7f9
Djalma-Neto <djalma7743@gmail.com>
2023-01-07 12:08:08 -0300

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const appWs = require('./app-ws');
  
-const server = app.listen(3300, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
     console.log(`App Rodando!`);
 })
  
