224cacfb4f568900f88138ce6d0c65fc0bcb947f
Djalma-Neto <djalma7743@gmail.com>
2023-01-07 12:41:36 -0300

    .

--- app-ws.js
+++ app-ws.js
@@ -1,15 +1,24 @@
 const WebSocket = require('ws');
 
+function getId(){
+    let s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
 module.exports = (server) => {
     const wss = new WebSocket.Server({
         server
     });
 
     wss.on('connection', (ws, req)=>{
+        ws.id = getId();
         ws.on('message', data=>{
             // ws.send(`recebido: "${data}"`);
             wss.clients.forEach(function(client) {
-                client.send(data.toString());
+                if(client.id != ws.id){
+                    client.send(data.toString());
+                }
              });
         });
         ws.on('error', error=>{
